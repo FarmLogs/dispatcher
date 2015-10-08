46b7b57dc82f6fbbf4510bf405a825fd163401fd
Frank Burnham <frank.e.burnham@gmail.com>
2015-10-08 11:49:03 -0400

    Add s3 repo

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject dispatcher "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Library to start specific workers in a jar"
+  :min-lein-version "2.0.0"
+  :plugins [[s3-wagon-private "1.1.2"]]
+  :repositories {"farmlogs-internal" {:url "s3p://fl-maven-repo/mvn"
+                                      :username ~(System/getenv "AMAZON_KEY")
+                                      :passphrase ~(System/getenv "AMAZON_SECRET")}}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main dispatcher.core
   :target-path "target/%s"
