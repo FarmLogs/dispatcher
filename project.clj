60e7f7438d0d507416f13ef913412cc1ec6e5015
Toby Crawley <toby@tcrawley.org>
2017-10-04 13:36:06 -0400

    Remove logging dep and prep for release

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
-(defproject com.farmlogs/dispatcher "1.0.1"
+(defproject com.farmlogs/dispatcher "1.0.2-SNAPSHOT"
   :description "Library to start specific workers in a jar"
   :min-lein-version "2.0.0"
   :plugins [[s3-wagon-private "1.2.0"]]
-  :repositories {"farmlogs-internal" {:url "s3p://fl-maven-repo/mvn"
-                                      :username ~(System/getenv "AMAZON_KEY")
-                                      :passphrase ~(System/getenv "AMAZON_SECRET")}}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.3.1"]]
-  :main dispatcher.core
-  :profiles {:uberjar {:aot :all}})
+  :repositories {"releases" {:url        "s3p://fl-maven-repo/mvn"
+                             :username   :env/AMAZON_KEY
+                             :passphrase :env/AMAZON_SECRET}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}})
--- src/dispatcher/exceptions/uncaught.clj
+++ src/dispatcher/exceptions/uncaught.clj
@@ -1,13 +1,14 @@
 (ns dispatcher.exceptions.uncaught
-  (:require [clojure.tools.logging :as log]
-            [dispatcher.exceptions.protocols :as p]))
+  (:require [dispatcher.exceptions.protocols :as p]))
 
 ;; Define default behavior for all exceptions. This can be overridden
 ;; by downstream code. It can also be extended to more specific types.
 (extend-protocol p/HandleException
   Throwable
   (handle [exception thread]
-    (log/error exception "Uncaught error on thread:" (.getName thread))))
+    (binding [*out* *err*]
+      (println "Uncaught error on thread:" (.getName thread)))
+    (.printStackTrace exception)))
 
 (defn register-uncaught-exception-handler
   ([]
